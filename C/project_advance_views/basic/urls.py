793aa1df53a558c9b10c7536dac26f630ae67e47
Charisma Fadzri Triprakoso <fadzri@alterra.id>
2019-12-10 23:14:34 +0700

    update form challenge 4

--- C/project_advance_views/basic/urls.py
+++ C/project_advance_views/basic/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('blog', views.blog, name = 'blog'),
     path('mentor', views.mentor, name = 'mentor'),
     path('mentee', views.mentee, name = 'mentee'),
-    path('author', views.author, name = 'author')
+    path('author', views.author, name = 'author'),
+    path('input_content', views.input_content, name='input_content'),
+    path('save_content', views.save_content, name='save_content')
 ]
--- C/project_advance_views/basic/views.py
+++ C/project_advance_views/basic/views.py
@@ -21,3 +21,17 @@ def mentee(request):
 
 def author(request):
     return render(request, 'basic/author.html', {})
+
+def input_content(request):
+    return render(request, 'basic/input_content.html', {})
+
+def save_content(request):
+    img_path = request.POST['img_path']
+    blog_title = request.POST['title']
+    blog_content = request.POST['blog_content']
+
+    bl = Blog(img_path=img_path, blog_title = blog_title, blog_content=blog_content)
+    bl.save()
+    blog = Blog.objects.all()
+    
+    return render(request,'basic/blog.html', {'blog': blog}) 
